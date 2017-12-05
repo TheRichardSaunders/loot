e0ca95a162fced27f45482517c1dc2c2c8878b1d
Richard Saunders <richard.saunders@bs215.org>
2017-12-04 20:45:19 -0500

    more jawn

---
+++ scripts/sgalf.py
@@ -0,0 +1,13 @@
+"""
+Potential encoding algorithm to help keep a secret.
+
+0x1, hexify
+0x2, front to back, back to front
+0x3, base something
+0x4, do something
+
+Divided we are 0x1, 0x2, 0x3, and 0x4.
+"""
+
+
+
